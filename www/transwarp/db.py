9ae92a081fdcf3512941339729f16a154e8d49de
Winann <songyingan@zhaoonline.com>
2015-01-08 14:51:28 +0800

    copy db.py success

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -7,8 +7,6 @@ __author__ = 'Winann Song'
 Database operation module.
 '''
 
-#db.py
-
 import time, uuid, functools, threading, logging
 
 # Dict object:
@@ -88,8 +86,9 @@ class _LasyConnection(object):
 	def cursor(self):
 		if self.connection is None:
 			connection = engine.connect()
-			logging.info('open connection <%s>...' % hex(id(connection)))
+			logging.info('open connection <%s>... With connection : %s' % (hex(id(connection)), type(connection)))
 			self.connection = connection
+		logging.info('cursor=====> %s' % type(self.connection.cursor()))
 		return self.connection.cursor()
 
 
@@ -109,27 +108,10 @@ class _LasyConnection(object):
 
 
 # 持有数据库连接的上下文对象：
-# class _DbCtx(threading.local):
-# 	def __init__(self):
-# 		self.connection = None
-# 		self.transactions = 0
-
-# 	def is_init(self):
-# 		return not self.connection is None
-
-# 	def init(self):
-# 		self.connection = _LasyConnection()
-# 		self.transactions = 0
-
-# 	def cleanup(self):
-# 		self.connection.cleanup()
-# 		self.connection = None
-
-# 	def cursor(self):
-# 		return self.connection.cursor()
-
 class _DbCtx(threading.local):
-	"""Thread local object that holds connection info"""
+	'''
+	Thread local object that holds connection info
+	'''
 	def __init__(self):
 		self.connection = None
 		self.transactions = 0
@@ -137,7 +119,7 @@ class _DbCtx(threading.local):
 	def is_init(self):
 		return not self.connection is None
 
-	def init (self):
+	def init(self):
 		logging.info('open lazy connection...')
 		self.connection = _LasyConnection()
 		self.transactions = 0
@@ -147,7 +129,9 @@ class _DbCtx(threading.local):
 		self.connection = None
 
 	def cursor(self):
-		''' Return cursor '''
+		'''
+		Return cursor
+		'''
 		return self.connection.cursor()
 
 
@@ -160,8 +144,10 @@ engine = None;
 class _Engine(object):
 	def __init__(self, connect):
 		self._connect = connect
+		# logging.info('self._connect: %s' % type(self._connect))
 	def connect(self):
-		return self._connect
+		logging.info('self._connect: %s' % type(self._connect))
+		return self._connect()		# 3.少写括号
 
 def create_engine(user, password, database, host = '127.0.0.1', port = 3306, **kw):
 	import mysql.connector
@@ -180,7 +166,7 @@ def create_engine(user, password, database, host = '127.0.0.1', port = 3306, **k
 	params['buffered'] = True
 	engine = _Engine(lambda: mysql.connector.connect(**params))
 	# test connection...
-	logging.info('Init mysql engine <%s> OK.' % hex(id(engine)))
+	logging.info('Init mysql engine <%s> OK.---->engine.connect type: %s' % (hex(id(engine)), type(engine.connect)))
 
 
 # 实现数据库连接的上下文，目的是自动获取和释放连接：
@@ -245,10 +231,12 @@ class _TransactionCtx(object):
 	def __enter__(self):
 		global _db_ctx
 		self.should_close_conn = False
-		if not _db_ctx.is_init:
+		if not _db_ctx.is_init():		# 1.忘写括号了
+			# needs open a connection first:
 			_db_ctx.init()
 			self.should_close_conn = True
 		_db_ctx.transactions = _db_ctx.transactions + 1
+		logging.info('begin transaction ...' if _db_ctx.transactions == 1 else 'join current transaction...')
 		return self
 
 	def __exit__(self, exctype, excvalue, traceback):
@@ -266,16 +254,22 @@ class _TransactionCtx(object):
 
 	def commit(self):
 		global _db_ctx
+		logging.info('commit transaction...')
 		try:
 			_db_ctx.connection.commit()
+			logging.info('commit ok.')
 		except:
+			logging.warning('commit failed. try rollback...')
 			_db_ctx.connection.rollback()
+			logging.warning('rollback ok.')
 			raise
 
 
 	def rollback(self):
 		global _db_ctx
+		logging.warning('rollback transaction...')
 		_db_ctx.connection.rollback()
+		logging.info('rollback ok.')
 
 def transaction():
 	'''
@@ -344,13 +338,13 @@ def _select(sql, first, *args):
 		cursor = _db_ctx.connection.cursor()
 		cursor.execute(sql, args)
 		if cursor.description:
-			names = [x[0] for x in cursor.descripion]
+			names = [x[0] for x in cursor.description]  # 6. descripion
 		if first:
 			values = cursor.fetchone()
 			if not values:
 				return None
 			return Dict(names, values)
-		return [Dict(name, x) for x in cursor.fetchall()]
+		return [Dict(names, x) for x in cursor.fetchall()]
 	finally:
 		if cursor:
 			cursor.close()
@@ -406,7 +400,7 @@ def select_int(sql, *args):
 	d = _select(sql, True, *args)
 	if len(d) != 1:
 		raise MultiColumnsError('Expect only one column.')
-		return d.values()[0]
+	return d.values()[0]		# 2. 缩进问题
 
 
 @with_connection
@@ -442,13 +436,16 @@ def _update(sql, *args):
 	sql = sql.replace('?', '%s')
 	logging.info('SQL: %s, ARGS: %s' % (sql, args))
 	try:
+		logging.info('up cursor')
 		cursor = _db_ctx.connection.cursor()
+		logging.info('down cursor')
 		cursor.execute(sql, args)
 		r = cursor.rowcount
-		if _db_ctx.transation == 0:
+		logging.info('r = %s' % r)
+		if _db_ctx.transactions == 0:		# 4. 少写s
 			# no transaction enviroment:
 			logging.info('auto commit')
-			_db_ctx.connecton.commit()
+			_db_ctx.connection.commit()		# 少写i(connecton)
 		return r
 	finally:
 		if cursor:
@@ -470,7 +467,7 @@ def insert(table, **kw):
     IntegrityError: 1062 (23000): Duplicate entry '2000' for key 'PRIMARY'
     '''
 	cols, args = zip(*kw.iteritems())
-	sql = 'insert into `%s` (%s) values (%s)' % (table, ', '.join(['`%s`' % col for col in cols]), ','.join(['?' for i in range(len(cols))]))
+	sql = 'insert into `%s` (%s) values (%s)' % (table, ','.join(['`%s`' % col for col in cols]), ','.join(['?' for i in range(len(cols))]))
 	return _update(sql, *args)
 
 def update(sql, *args):
